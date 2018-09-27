95b73b7ff9be10fb8abee5bce429e46ee36174cb
Mary Jack <maryjackinthebox@gmail.com>
2018-09-27 20:17:34 +0000

    automatically backed up by learn

---
+++ db/migrate/20180924162737_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration
+ActiveRecord::Migration
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+  end
+ 
+  def down
+    drop_table :dogs
+  end
+end
+
